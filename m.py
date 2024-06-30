7e7aab4eec56cc23f8d3455a75cb2d8fd3640e88
Dvijkumar Patel <95489688+DvijK@users.noreply.github.com>
2024-06-30 08:20:31 +0530

    Add files via upload

--- m.py
+++ m.py
@@ -8,7 +8,7 @@ import os
 from keep_alive import keep_alive
 keep_alive()
 # insert your Telegram bot token here
-bot = telebot.TeleBot('7302767710:AAEaNWnyJqF2KTNpc7OsY4x-rke4Wd0yMfc')
+bot = telebot.TeleBot('7104989151:AAHCWV57ZHSZNGT2CavyIcJxYXzIv1ymhyY')
 
 # Admin user IDs
 admin_id = ["5083353662"]
--- venom.py
+++ venom.py
@@ -16,11 +16,11 @@ from telebot.types import ReplyKeyboardMarkup, KeyboardButton
 
 loop = asyncio.get_event_loop()
 
-TOKEN = '6354268649:AAHcUoRAuRfVmFzAhLCyu9d2R4XO6A_XGsU'
+TOKEN = '7104989151:AAHCWV57ZHSZNGT2CavyIcJxYXzIv1ymhyY'
 MONGO_URI = 'mongodb+srv://VENOMxCRAZY:CRAZYxVENOM@cluster0.ythilmw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0&tlsAllowInvalidCertificates=true'
-FORWARD_CHANNEL_ID = -1002224570220
-CHANNEL_ID = -1002224570220
-error_channel_id = -1002224570220
+FORWARD_CHANNEL_ID = -1002162586488
+CHANNEL_ID = -1002162586488
+error_channel_id = -1002162586488
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
